92ed909abf4ffbb3f8019ee9afb3a0736dcd72c5
Mattias <mattiasjohansson.vfx@gmail.com>
2024-09-03 17:16:00 +0200

    Add link to go back to home on NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export function NotFound() {
   return (
     <main>
       <h1>Page not found (404)</h1>
+      <Link to="/">Go Home</Link>
     </main>
   )
 }
